5ca619845f837cb8f59d7425466464fdf4c65455
Leonel Rivero <103612900+leorivero91@users.noreply.github.com>
2022-05-18 17:26:28 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let num1 : number = prompt ("Ingrese el primer numero"));
+let num2 : number = prompt ("Ingrese el segundo numero"));
+
+function esMultiplo(num1: number,num2:number): boolean {
+  return ((num1 % num2) ==0);
+};
+
+if(esMultiplo(num1,num2)){
+  console.log(num1, "Es multiplo de :" , num2);
+  } else {
+   console.log(num1, "No es multiplo de :", num2);
+  };
